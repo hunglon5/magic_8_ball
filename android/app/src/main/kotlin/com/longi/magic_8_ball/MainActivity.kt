a6573cd42bf24b6670d50ef08fe53ef8a86dedfc
admin <ingkeanglong@gmail.com>
2024-11-08 11:46:24 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/longi/magic_8_ball/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.longi.magic_8_ball
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() => runApp(const MaterialApp(
+      home: BallPage(),
+    ));
+
+class BallPage extends StatelessWidget {
+  const BallPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          backgroundColor: Colors.blue.shade900,
+          title: const Text(
+            'Ask Me Anything',
+            style: TextStyle(color: Colors.white, fontSize: 20.0),
+          ),
+        ),
+        body: const Ball(),
+      ),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  const Ball({super.key});
+
+  @override
+  State<Ball> createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextButton(
+        onPressed: () {
+          setState(() {
+            ballNumber = Random().nextInt(5) + 1;
+          });
+        },
+        child: Image.asset('images/ball$ballNumber.png'),
+      ),
+    );
+  }
+}
